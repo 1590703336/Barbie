eb0353924fc8d78b5701745138beeb6e2bce9d89
Maazin2007 <maazinkhattak74@gmail.com>
2025-12-29 16:11:38 -0700

    created server.js

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -1,9 +1,7 @@
 import express from "express";
-import { PORT } from "./config/env.js";
 import userRouter from "./modules/user/user.routes.js";
 import authRouter from "./modules/auth/auth.routes.js";
 import subscriptionRouter from "./modules/subscription/subscription.routes.js";
-import connectToDatabase from "./database/mongodb.js";
 import errorMiddlewares from "./middlewares/error.middlewares.js";
 import cookieParser from "cookie-parser";
 import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
@@ -26,10 +24,5 @@ app.get('/', (req, res) => {
     res.send('Welcome to the app!');
 });
 
-app.listen(PORT, async () => {
-    console.log(`Server running on http://localhost:${ PORT }`);
-
-    await connectToDatabase()
-});
 
 export default app;
---
+++ apps/backend/src/server.js
@@ -0,0 +1,9 @@
+import app from "./app.js";
+import { PORT } from "./config/env.js";
+import connectToDatabase from "./database/mongodb.js";
+
+app.listen(PORT, async () => {
+    console.log(`Server running on http://localhost:${ PORT }`);
+
+    await connectToDatabase()
+});
